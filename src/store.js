df3ee53125482752e40c904b9a09fdc89784ec13
Fawad Memon <meetfawad.online@gmail.com>
2021-01-26 18:53:57 +0500

    fix issue when redux extension not available

--- src/store.js
+++ src/store.js
@@ -8,7 +8,7 @@ export default createStore(
     reducers,
     compose(
         applyMiddleware(rootEpic),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()    
+        // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()    
     )
 )
 
